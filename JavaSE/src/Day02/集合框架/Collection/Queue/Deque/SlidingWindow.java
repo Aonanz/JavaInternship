0c9a5b59769e7e722ff484a0fc50540e0058b80d
Aonan Zhang <aonanz@student.unimelb.edu.au>
2021-02-02 17:44:07 +0800

    ArrayList&LinkedList

--- JavaSE/src/Day02/集合框架/Collection/Queue/Deque/SlidingWindow.java
+++ JavaSE/src/Day02/集合框架/Collection/Queue/Deque/SlidingWindow.java
@@ -4,6 +4,7 @@ import java.util.Deque;
 import java.util.LinkedList;
 
 // 返回nums数组中每个大小为k的滑动窗口的最大值
+// 单调队列 时间O(n),空间O(k)
 public class SlidingWindow {
     public int[] maxSlidingWindow(int[] nums, int k) {
 
@@ -13,7 +14,7 @@ public class SlidingWindow {
         // 初始化双向联表和返回数组
         Deque<Integer> deque = new LinkedList<>();
         int[] res = new int[nums.length - k + 1];
-        
+
         for(int j = 0, i = 1 - k; j < nums.length; i++, j++) {
             // 删除 deque 中对应的 nums[i-1]
             if(i > 0 && deque.peekFirst() == nums[i - 1])
